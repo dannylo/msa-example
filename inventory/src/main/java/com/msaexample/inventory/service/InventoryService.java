59af0bd26e965794ada86a2eed90e9010e5e038b
Dannylo Johnathan <dannylojohnathan@gmail.com>
2019-10-08 20:38:40 -0300

    Finalização do serviço basico de inventário.

--- inventory/src/main/java/com/msaexample/inventory/service/InventoryService.java
+++ inventory/src/main/java/com/msaexample/inventory/service/InventoryService.java
@@ -1,5 +1,7 @@
 package com.msaexample.inventory.service;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,7 @@ public class InventoryService {
 	private InventoryRepository repository;
 
 	public Inventory save(Inventory inventory) {
+		inventory.setLastUpdated(LocalDate.now());
 		return repository.save(inventory);
 	}
 
